d159a80083cfd7f302d9e8b60aa23a9bd31caa5f
Donald Pakkies <donaldpakkies@gmail.com>
2018-08-17 15:55:22 +0200

    Feat: added object class for helpers

---
+++ Support/Obj.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AtlantisPHP\ElComponent\Support;
+
+use AtlantisPHP\ElComponent\Html;
+
+class Obj
+{
+  /**
+   * el
+   *
+   * @param string $element
+   * @return void
+   */
+  public static function el(string $element) : object
+  {
+    switch ($element) {
+      case 'form':
+        return Html::form();
+        break;
+      
+      default:
+        # code...
+        break;
+    }
+  }
+}
