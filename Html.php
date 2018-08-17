3893a385f0052e4563f80039c85b65f845d79649
Donald Pakkies <donaldpakkies@gmail.com>
2018-08-17 15:52:35 +0200

    Feat: added a new Html class for creating html elements

---
+++ Html.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AtlantisPHP\ElComponent;
+
+use AtlantisPHP\ElComponent\Elements\Form;
+
+class Html
+{
+  /**
+   * form
+   *
+   * @return void
+   */
+  public static function form()
+  {
+    return new Form;
+  }
+}
