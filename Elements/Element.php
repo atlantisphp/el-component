ea184c787930398958a1935931433a0c93280647
Donald Pakkies <donaldpakkies@gmail.com>
2018-08-17 16:43:54 +0200

    Refactor: moved some methods to the Element class

--- Elements/Element.php
+++ Elements/Element.php
@@ -12,6 +12,57 @@ class Element
   public $builder;
 
   /**
+   * text
+   *
+   * @param mixed array
+   * @return void
+   */
+  public function text(array $attributes = []) : object
+  {
+    $input = 'input type="text"';
+    foreach ($attributes as $key => $value) {
+      $input = $input . ' ' . $key . '="' . $value . '" ';
+    }
+
+    $this->builder = $this->builder . "<$input />";
+    return $this;
+  }
+
+  /**
+   * password
+   *
+   * @param mixed array
+   * @return void
+   */
+  public function password(array $attributes = []) : object
+  {
+    $input = 'input type="password"';
+    foreach ($attributes as $key => $value) {
+      $input = $input . ' ' . $key . '="' . $value . '" ';
+    }
+
+    $this->builder = $this->builder . "<$input />";
+    return $this;
+  }
+
+  /**
+   * input
+   *
+   * @param mixed array
+   * @return object
+   */
+  public function input(array $attributes = []) : object
+  {
+    $input = 'input';
+    foreach ($attributes as $key => $value) {
+      $input = $input . ' ' . $key . '="' . $value . '" ';
+    }
+
+    $this->builder = $this->builder . "<$input />";
+    return $this;
+  }
+
+  /**
    * br
    *
    * @param mixed array
